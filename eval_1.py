ced3633b5954be0d50a95c30569e738847fb8bc2
fAIza-AU <bamideleolaoye807@gmail.com>
2026-08-19 04:50:01 +0100

    Day 4: iterate on prompt, add README

--- eval_1.py
+++ eval_1.py
@@ -1,18 +1,37 @@
 from bb import ask_llm
 from judge import groq_llm
 import json 
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_key = os.environ.get('SECRET_KEY')
+
+client = Groq(api_key=my_key)
 with open("test_cases.json") as f:
     all_cases = json.load(f)
+
+def eval_llm (article,limit):
+    response = client.chat.completions.create(
+        model="openai/gpt-oss-120b",
+        messages=[
+            {'role': 'system', 'content': f"You are a professional writer. Summarize the article without exceeding {limit} words. Do not use bullet points or headers, do not exceed it in any circumstances, even if it mean dropping unneccessary words that wont break the summary."},
+            {'role': 'user', 'content': article}
+        ]
+    )
+    return response.choices[0].message.content
+  
 
 for case in all_cases:
     print(case['id'])
     article = case['source_text']
-    
-    summary = ask_llm(article)
+    limit = case['must_not_exceed_words']
+    summary = eval_llm(article, limit)
     summary = summary.replace("\u202f", " ")
     words = summary.split()
     length = len(words)
-    limit = case['must_not_exceed_words']
+   
     if length > limit:
         print("FAIL - too long")
     else:
@@ -26,7 +45,4 @@ for case in all_cases:
     print('JUDGE SAYS:', Question)
     print(summary)
     print('*********************')
-  
-
-
 
